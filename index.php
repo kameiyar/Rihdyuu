d299d0d17187dc7a9f7fd9ab955a505b4318d695
kameiyar <ahmad1250wxdadzh@gmail.com>
2025-05-12 20:40:20 +0330

    index.html

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ip = $_SERVER['REMOTE_ADDR'];
-$token = "7122462118:AAGdnDrTtLv6kYzROabnkaT-_9W54WJeXBQ";
-$chat_id = "1196450049";
-$text = "New visitor IP address: $ip";
-
-file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=" . urlencode($text));
-echo "Your IP address is: $ip";
-?>
